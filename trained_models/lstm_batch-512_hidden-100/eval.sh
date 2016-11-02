03b0903af8c9f23034a6216f748f2010a4534332
Nicolas ANGELARD-GONTIER <nicolas.angelard-gontier@mail.mcgill.ca>
2016-11-02 11:11:35 -0400

    added evalulation code into main.py and added evaluation scripts

--- trained_models/lstm_batch-512_hidden-100/eval.sh
+++ trained_models/lstm_batch-512_hidden-100/eval.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-THEANO_FLAGS='floatX=float32,device=gpu0' python main.py \
+THEANO_FLAGS='floatX=float32,device=gpu0' python main_with-eval.py \
     --encoder 'lstm' \
     --batch_size 512 \
-    --hidden_size 200 \
+    --hidden_size 100 \
     --optimizer 'adam' \
     --lr 0.001 \
     --fine_tune_W True \
     --fine_tune_M True \
-    --input_dir './twitter_dataset' \
+    --input_dir '../../twitter_dataset' \
     --dataset_fname 'dataset_twitter_bpe.pkl' \
     --W_fname 'W_twitter_bpe.pkl' \
     --n_epochs 5 \
-    --save_model True
+    --test True
 
--- trained_models/lstm_batch-512_hidden-200/eval.sh
+++ trained_models/lstm_batch-512_hidden-200/eval.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-THEANO_FLAGS='floatX=float32,device=gpu0' python main.py \
+THEANO_FLAGS='floatX=float32,device=gpu0' python main_with-eval.py \
     --encoder 'lstm' \
     --batch_size 512 \
     --hidden_size 200 \
@@ -8,9 +8,9 @@ THEANO_FLAGS='floatX=float32,device=gpu0' python main.py \
     --lr 0.001 \
     --fine_tune_W True \
     --fine_tune_M True \
-    --input_dir './twitter_dataset' \
+    --input_dir '../../twitter_dataset' \
     --dataset_fname 'dataset_twitter_bpe.pkl' \
     --W_fname 'W_twitter_bpe.pkl' \
     --n_epochs 5 \
-    --save_model True
+    --test True
 
