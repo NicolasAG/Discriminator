93f49a24754d16fd5d6e8d723f22c4e2e413ef1e
Nicolas ANGELARD-GONTIER <nicolas.angelard-gontier@mail.mcgill.ca>
2017-04-16 15:54:06 -0400

    removed unused duplicated files

--- eval.sh
+++ eval.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-THEANO_FLAGS='floatX=float32,device=gpu0' python main_with-eval.py \
+THEANO_FLAGS='floatX=float32,device=gpu0' python main.py \
     --encoder 'lstm' \
     --batch_size 512 \
     --hidden_size 100 \
@@ -8,7 +8,7 @@ THEANO_FLAGS='floatX=float32,device=gpu0' python main_with-eval.py \
     --lr 0.001 \
     --fine_tune_W True \
     --fine_tune_M True \
-    --input_dir '../../twitter_dataset' \
+    --input_dir './twitter_dataset' \
     --dataset_fname 'dataset_twitter_bpe.pkl' \
     --W_fname 'W_twitter_bpe.pkl' \
     --test True
