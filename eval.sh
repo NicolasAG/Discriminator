54e74644f8cf72607c80541fc3922d45ee34d976
Nicolas ANGELARD-GONTIER <nicolas.angelard-gontier@mail.mcgill.ca>
2017-05-11 17:35:08 -0400

    added plots & results for each specialized discriminators

--- eval.sh
+++ eval.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-THEANO_FLAGS='floatX=float32,device=gpu0' python main.py \
+THEANO_FLAGS='floatX=float32,device=gpu2' python main.py \
     --data_path './twitter_dataset' \
     --dataset_fname 'dataset_HREDx2-VHREDx2-cTFIDF-RND-TRUE_twitter_bpe.pkl' \
-    --W_fname 'W_HREDx2-VHREDx2-cTFIDF-RND-TRUE_300_twitter_bpe.pkl' \
+    --W_fname 'W_300_twitter_bpe.pkl' \
+    --load_path './trained_models/lstm-100_Hx2_Vx2_c-TFIDF_RND_TRUE' \
     --load_prefix 'lstm-100_adam_HREDx2-VHREDx2-cTFIDF-RND-TRUE_twitter' \
     --test True \
     --plot_human_scores True \
