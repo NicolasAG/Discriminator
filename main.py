dd4fb9265c0620cadf922f7088ae5378cdbc0aa0
NicolasAG <gontier.nicolas@yahoo.fr>
2017-05-30 17:02:52 -0400

    added logging & handle model loading exception

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from __future__ import division
+import logging
 import argparse
 import cPickle
 import numpy as np
@@ -10,6 +11,9 @@ sys.setrecursionlimit(10000)
 
 from model import Model as DE_Model
 
+logger = logging.getLogger(__name__)
+
+
 def string2indices(p_str, str_to_idx):
     """
     Lookup dictionary from word to index to retrieve the list of indices from a string of words.
@@ -19,6 +23,23 @@ def string2indices(p_str, str_to_idx):
     :return: a new list of indices corresponding to the given string of words.
     """
     return [str_to_idx[w] for w in p_str.strip().split() if w in str_to_idx]
+
+def process_dialogues(dialogues):
+    """
+    get the list of contexts, and the list of TRUE responses.
+    Removes </d> </s> at end, splits into contexts/ responses
+    """
+    contexts = []
+    responses = []
+    for d in dialogues:
+        d_proc = d[:-3]
+        index_list = [i for i, j in enumerate(d_proc) if j == 1]
+        split = index_list[-1] + 1
+        context = filter(lambda idx: idx != 1, d_proc[:split])  # remove </s> from context
+        contexts.append(context)
+        response = filter(lambda idx: idx != 1, d_proc[split:])  # remove </s> from response
+        responses.append(response)
+    return contexts, responses
 
 def sort_by_len(dataset):
     """
@@ -32,10 +53,42 @@ def sort_by_len(dataset):
     for k in ['c', 'r', 'y']:
         dataset[k] = np.array(dataset[k])[indices]
 
+def create_model(data, w, args):
+    return DE_Model(
+        data=data,
+        W=w.astype(theano.config.floatX),
+        save_path=args.save_path,
+        save_prefix=args.save_prefix,
+        max_seqlen=args.max_seqlen,                         # default 160
+        batch_size=args.batch_size,                         # default 256
+        # Network architecture:
+        encoder=args.encoder,                               # default RNN
+        hidden_size=args.hidden_size,                       # default 200
+        n_recurrent_layers=args.n_recurrent_layers,         # default 1
+        is_bidirectional=args.is_bidirectional,             # default False
+        # Learning parameters:
+        patience=args.patience,                             # default 10
+        optimizer=args.optimizer,                           # default ADAM
+        lr=args.lr,                                         # default 0.001
+        lr_decay=args.lr_decay,                             # default 0.95
+        fine_tune_W=args.fine_tune_W,                       # default False
+        fine_tune_M=args.fine_tune_M,                       # default False
+        # NTN parameters:
+        use_ntn=args.use_ntn,                               # default False
+        k=args.k,                                           # default 4
+        # Regularization parameters:
+        penalize_emb_norm=args.penalize_emb_norm,           # default False
+        penalize_emb_drift=args.penalize_emb_drift,         # default False
+        emb_penalty=args.emb_penalty,                       # default 0.001
+        penalize_activations=args.penalize_activations,     # default False
+        act_penalty=args.act_penalty                        # default 500
+    )
+
 def str2bool(v):
     return v.lower() in ("yes", "true", "t", "1")
 
 def main():
+    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s: %(name)s: %(levelname)s: %(message)s", stream=sys.stdout)
     parser = argparse.ArgumentParser()
     parser.register('type', 'bool', str2bool)
 
@@ -97,19 +150,27 @@ def main():
     parser.add_argument('--plot_learning_curves', type='bool', default=False, help='Plot train & val learning curves')
 
     args = parser.parse_args()
-    print 'args:', args
+    logger.info('args: %s' % args)
+    # Saving args to file
+    with open("%s/%s_args.pkl" % (args.save_path, args.save_prefix), 'wb') as handle:
+        cPickle.dump(args, handle, protocol=cPickle.HIGHEST_PROTOCOL)
+    logger.info('saved.')
+
     np.random.seed(args.seed)
 
-    print "\nLoading data..."
+    logger.info("")
+    logger.info("Loading data...")
     # data sets are dictionaries containing contexts, responses, flag
-    train_data, val_data, test_data = cPickle.load(open('%s/%s' % (args.data_path, args.dataset_fname), 'rb'))
+    with open('%s/%s' % (args.data_path, args.dataset_fname), 'rb') as handle:
+        train_data, val_data, test_data = cPickle.load(handle)
     # W is the word embedding matrix and word2idx is a dictionary.
-    W, word2idx, idx2word = cPickle.load(open('%s/%s' % (args.data_path, args.W_fname), 'rb'))
-    print "W.shape:", W.shape  # (5092,300) = word embedding for each vocab word
+    with open('%s/%s' % (args.data_path, args.W_fname), 'rb') as handle:
+        W, word2idx, idx2word = cPickle.load(handle)
+    logger.info("W.shape: %s" % (W.shape,))  # (5092,300) = word embedding for each vocab word
 
-    print "Number of training examples: %d" % (len(train_data['c']))
-    print "Number of validation examples: %d" % (len(val_data['c']))
-    print "Number of test examples: %d" % (len(test_data['c']))
+    logger.info("Number of training examples: %d" % len(train_data['c']))
+    logger.info("Number of validation examples: %d" % len(val_data['c']))
+    logger.info("Number of test examples: %d" % len(test_data['c']))
 
     # Cap the number of training examples
     if args.train_examples:
@@ -117,61 +178,68 @@ def main():
         train_data['c'] = train_data['c'][:num_train_examples]
         train_data['r'] = train_data['r'][:num_train_examples]
         train_data['y'] = train_data['y'][:num_train_examples]
-        print('New number of training examples: %d' % (len(train_data['c'])))
+        logger.info('New number of training examples: %d' % len(train_data['c']))
 
     data = {'train': train_data, 'val': val_data, 'test': test_data}
-    print("data loaded!")
+    logger.info("data loaded!")
 
     # sort the training data by context length
     if args.sort_by_len:
         sort_by_len(data['train'])
 
     if args.resume or args.test or args.retrieve:
-        print "\nLoading model..."
-        with open('%s/%s_model.pkl' % (args.load_path, args.load_prefix), 'rb') as handle:
-            model = cPickle.load(handle)
-            model.data = data  # in cases when we want to test or resume with new data
+        logger.info("")
+        logger.info("Loading model...")
+        try:
+            with open('%s/%s_model.pkl' % (args.load_path, args.load_prefix), 'rb') as handle:
+                model = cPickle.load(handle)
+                model.data = data  # in cases when we want to test or resume with new data
+        except cPickle.UnpicklingError:
+            logger.info("cPickle.UnpicklingError: couldn't load the model")
+            # Loading old arguments
+            with open('%s/%s_args.pkl' % (args.load_path, args.load_prefix), 'rb') as handle:
+                old_args = cPickle.load(handle)
+            
+            logger.info("Creating a new one...")
+            model = create_model(data, W, old_args)
+
+            logger.info("Set the learned weights...")
+            with open('%s/%s_best_weights.pkl' % (args.load_path, args.load_prefix), 'rb') as handle:
+                params = cPickle.load(handle)
+                lasagne.layers.set_all_param_values(model.l_out, params)
+            with open('%s/%s_best_M.pkl' % (args.load_path, args.load_prefix), 'rb') as handle:
+                M = cPickle.load(handle)
+                model.M.set_value(M)
+            with open('%s/%s_best_embed' % (args.load_path, args.load_prefix), 'rb') as handle:
+                em = cPickle.load(handle)
+                model.embeddings.set_value(em)
+
+            logger.info("Testing the model...")
+            model.test()
+            ok = raw_input("\nDoes the performance look good? If so, will save the model. (y/n): ")
+            if ok == 'y':
+                logger.info("Saving new model in %s/%s_model.pkl" % (args.save_path, args.save_prefix))
+                with open("%s/%s_model.pkl" % (args.save_path, args.save_prefix), 'wb') as handle:
+                    cPickle.dump(model, handle, protocol=cPickle.HIGHEST_PROTOCOL)
+                logger.info("Saved.")
+            else:
+                logger.info("Not saved.")
+
         with open('%s/%s_timings.pkl' % (args.load_path, args.load_prefix), 'rb') as handle:
             timings = cPickle.load(handle)
             model.timings = timings  # load last timings (when no improvement was done)
-        print "Model loaded."
+        logger.info("Model loaded.")
     else:
-        print "\nCreating model..."
-        model = DE_Model(
-            data=data,
-            W=W.astype(theano.config.floatX),
-            save_path=args.save_path,
-            save_prefix=args.save_prefix,
-            max_seqlen=args.max_seqlen,                         # default 160
-            batch_size=args.batch_size,                         # default 256
-            # Network architecture:
-            encoder=args.encoder,                               # default RNN
-            hidden_size=args.hidden_size,                       # default 200
-            n_recurrent_layers=args.n_recurrent_layers,         # default 1
-            is_bidirectional=args.is_bidirectional,             # default False
-            # Learning parameters:
-            patience=args.patience,                             # default 10
-            optimizer=args.optimizer,                           # default ADAM
-            lr=args.lr,                                         # default 0.001
-            lr_decay=args.lr_decay,                             # default 0.95
-            fine_tune_W=args.fine_tune_W,                       # default False
-            fine_tune_M=args.fine_tune_M,                       # default False
-            # NTN parameters:
-            use_ntn=args.use_ntn,                               # default False
-            k=args.k,                                           # default 4
-            # Regularization parameters:
-            penalize_emb_norm=args.penalize_emb_norm,           # default False
-            penalize_emb_drift=args.penalize_emb_drift,         # default False
-            emb_penalty=args.emb_penalty,                       # default 0.001
-            penalize_activations=args.penalize_activations,     # default False
-            act_penalty=args.act_penalty                        # default 500
-        )
-        print "Model created."
+        logger.info("")
+        logger.info("Creating model...")
+        model = create_model(data, W, args)
+        logger.info("Model created.")
 
     # Get correlation with Human scores:
     if args.plot_human_scores:
         # Load human scores
-        print "\nLoading human score data..."
+        logger.info("")
+        logger.info("Loading human score data...")
         with open('../data/twitter/human_scores-vhred_embeddings.pkl', 'rb') as handle:
             vhred_embeddings = cPickle.load(handle)
 
@@ -205,64 +273,36 @@ def main():
 
     # If testing a pre-saved model
     if args.test:
-        print "\nTesting the model..."
+        logger.info("")
+        logger.info("Testing the model...")
         model.test()
 
     # If retrieving responses from training set
     elif args.retrieve:
-        print "\nGenerating responses..."
-        # TODO: run tfidf retriever on model.data['train'] to get a smaller list of responses
-        retrieved_responses = model.retrieve('test', k=10, response_set=None)
+        logger.info("")
+        logger.info("Loading original twitter data...")
+        with open('%s/bpe/Train.dialogues.pkl' % args.data_path, 'rb') as handle:
+            train_conversations = cPickle.load(handle)
+        train_contexts, train_responses = process_dialogues(train_conversations)
+        logger.info("number of contexts: %d" % len(train_contexts))
+        logger.info("number of responses: %d" % len(train_responses))
+
+        logger.info("")
+        logger.info("Retrieving responses...")
+        retrieved_responses = model.retrieve(context_set=train_contexts, response_set=train_responses, k=10, batch_size=100)
+        with open("%s/%s_Train_retrieved_responses.pkl" % (args.save_path, args.save_prefix), 'wb') as handle:
+            cPickle.dump(retrieved_responses, handle, protocol=cPickle.HIGHEST_PROTOCOL)
+        logger.info('saved.')
 
     # If training the model
     else:
-        "\nTraining model..."
+        logger.info("")
+        logger.info("Training model...")
         test_perf = model.train(n_epochs=args.n_epochs, patience=args.patience, verbose=False)
-        "Model trained."
-        print "test_perfs =", test_perf
-        # print "test_probas =", test_probas
+        logger.info("Model trained.")
+        logger.info("test_perfs = %.9f" % test_perf)
+        # logger.info("test_probas = %.9f" % test_probas)
 
 if __name__ == '__main__':
     main()
-
-    ###
-    # DEBUG compute_embedding
-    ###
-    """
-    print "\nLoading original twitter data..."
-    dialogues = cPickle.load(open('./twitter_dataset/bpe/Train.dialogues.pkl', 'rb'))
-
-    # get the list of contexts, and the list of TRUE responses.
-    def process_dialogues(dialogues):
-        '''Removes </d> </s> at end, splits into contexts/ responses '''
-        contexts = []
-        responses = []
-        for d in dialogues:
-            d_proc = d[:-3]
-            index_list = [i for i, j in enumerate(d_proc) if j == 1]
-            split = index_list[-1] + 1
-            context = filter(lambda idx: idx != 1, d_proc[:split])  # remove </s> from context
-            contexts.append(context)
-            response = filter(lambda idx: idx != 1, d_proc[split:])  # remove </s> from response
-            responses.append(response)
-        return contexts, responses
-
-    contexts, true_responses = process_dialogues(dialogues)
-    print "number of contexts:", len(contexts)
-    print "number of responses:", len(true_responses)
-
-    n_batches = len(contexts) // model.batch_size
-    c_embs = []
-    for i in xrange(n_batches):
-        c_embs.extend(model.compute_context_embeddings(contexts, i))
-    print "number of c_embs:", len(c_embs)
-    print c_embs[0]
-
-    n_batches = len(true_responses) // model.batch_size
-    r_embs = []
-    for i in xrange(n_batches):
-        r_embs.extend(model.compute_response_embeddings(true_responses, i))
-    print "number of r_embs:", len(r_embs)
-    print r_embs[0]
-    """
 
