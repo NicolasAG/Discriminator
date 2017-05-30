485117d7f92533a14466536ac20eec44e42593b9
NicolasAG <gontier.nicolas@yahoo.fr>
2017-05-30 17:01:40 -0400

    using theano 0.8.2 flags

--- eval.sh
+++ eval.sh
@@ -3,10 +3,7 @@
 echo "VIRTUALENV = $VIRTUALENV"
 . $VIRTUALENV/bin/activate
 
-# nvidia-smi
-# gpu-who
-
-export THEANO_FLAGS=$THEANO_FLAGS,device=cuda
+export THEANO_FLAGS=$THEANO_FLAGS,device=gpu
 echo "THEANO_FLAGS = $THEANO_FLAGS"
 
 CUDA_VERSION="8.0"
@@ -14,12 +11,15 @@ export CUDA=/usr/local/cuda-${CUDA_VERSION}/bin
 export PATH=${CUDA}:$PATH
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-${CUDA_VERSION}/lib64
-export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/cuda-${CUDA_VERSION}/lib64
-export CPATH=$CPATH:/usr/local/cuda-${CUDA_VERSION}/lib64
+# export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/cuda-${CUDA_VERSION}/lib64
+# export CPATH=$CPATH:/usr/local/cuda-${CUDA_VERSION}/lib64
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${gpuarray}/lib
-export LIBRARY_PATH=$LIBRARY_PATH:${gpuarray}/lib
-export CPATH=$CPATH:${gpuarray}/include
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${gpuarray}/lib
+# export LIBRARY_PATH=$LIBRARY_PATH:${gpuarray}/lib
+# export CPATH=$CPATH:${gpuarray}/include
+
+# To use P100 GPUs:
+export CUDA_CACHE_PATH=/lm/scratch/nicolas_gontier/cuda_cache
 
 export HOME=/nlu/users/nicolas_gontier/home
 
@@ -39,6 +39,8 @@ python2.7 main.py \
     --W_fname 'W_300_twitter_bpe.pkl' \
     --load_path './trained_models/lstm-100_RND' \
     --load_prefix 'lstm-100_adam_RND-TRUE_twitter' \
+    --save_path '.' \
+    --save_prefix 'lstm-100_adam_RND-TRUE_twitter' \
     --test True \
-    --plot_response_length True \
-    --plot_learning_curves True
+    --plot_response_length False \
+    --plot_learning_curves False
--- train_discriminator.sh
+++ train_discriminator.sh
@@ -3,10 +3,7 @@
 echo "VIRTUALENV = $VIRTUALENV"
 . $VIRTUALENV/bin/activate
 
-# nvidia-smi
-# gpu-who
-
-export THEANO_FLAGS=$THEANO_FLAGS,device=cuda
+export THEANO_FLAGS=$THEANO_FLAGS,device=gpu
 echo "THEANO_FLAGS = $THEANO_FLAGS"
 
 CUDA_VERSION="8.0"
@@ -14,12 +11,15 @@ export CUDA=/usr/local/cuda-${CUDA_VERSION}/bin
 export PATH=${CUDA}:$PATH
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-${CUDA_VERSION}/lib64
-export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/cuda-${CUDA_VERSION}/lib64
-export CPATH=$CPATH:/usr/local/cuda-${CUDA_VERSION}/lib64
+# export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/cuda-${CUDA_VERSION}/lib64
+# export CPATH=$CPATH:/usr/local/cuda-${CUDA_VERSION}/lib64
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${gpuarray}/lib
-export LIBRARY_PATH=$LIBRARY_PATH:${gpuarray}/lib
-export CPATH=$CPATH:${gpuarray}/include
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${gpuarray}/lib
+# export LIBRARY_PATH=$LIBRARY_PATH:${gpuarray}/lib
+# export CPATH=$CPATH:${gpuarray}/include
+
+# To use P100 GPUs:
+export CUDA_CACHE_PATH=/lm/scratch/nicolas_gontier/cuda_cache
 
 export HOME=/nlu/users/nicolas_gontier/home
 
@@ -34,17 +34,21 @@ echo "HOME = $HOME"
 echo ""
 
 python2.7 main.py \
-    --data_path './twitter_dataset' \
-    --dataset_fname 'dataset_RND-TRUE_twitter_bpe.pkl' \
-    --W_fname 'W_300_twitter_bpe.pkl' \
-    --save_prefix 'lstm-100_adam_RND-TRUE_twitter' \
-    --batch_size 500 \
+    --data_path './ubuntu_dataset' \
+    --dataset_fname 'dataset_RND-TRUE_ubuntu_bpe.pkl' \
+    --W_fname 'W_300_ubuntu_bpe.pkl' \
+    --save_path '.' \
+    --save_prefix 'lstm-300_adam_RND-TRUE_ubuntu' \
+    --batch_size 256 \
     --encoder 'lstm' \
-    --hidden_size 100 \
+    --hidden_size 300 \
     --is_bidirectional False \
     --n_recurrent_layers 1 \
-    --patience 5 \
+    --patience 10 \
     --n_epochs 100 \
     --optimizer 'adam' \
     --fine_tune_W True \
-    --fine_tune_M True
+    --fine_tune_M True \
+    --load_path './trained_models/lstm-300_RND' \
+    --load_prefix 'lstm-300_adam_RND-TRUE_ubuntu' #\
+    #--resume True
